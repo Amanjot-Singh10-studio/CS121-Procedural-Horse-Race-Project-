a53a3a014bfc09937e255b46fe0ffdc12c910f86
Amanjot Singh <Amanjot.singh@bsu.edu>
2026-09-16 19:55:14 +0000

    horse race

---
+++ Horserace.cpp
@@ -0,0 +1,65 @@
+#include <iostream> 
+#include <cstdlib> 
+#include <ctime> 
+
+void advance(int horseNum, int* horses);
+void printLane(int horseNum, int* horses);
+bool isWinner(int horseNum, int* horses);
+
+int main() { 
+	int horses[5] = {0,0,0,0,0}; 
+	bool keepGoing = true; 
+	srand(time(NULL)); 
+	while (keepGoing) { 
+		for (int horse = 0; horse < 5; horse++) { 
+			advance(horse, horses); 
+			printLane(horse, horses); 
+			if (isWinner(horse, horses)) {
+				keepGoing = false; 
+			} 
+		} 
+		if (keepGoing) { 
+			std::cout << "Press Enter for another turn" << std::endl;  
+			std::cin.ignore(); 
+		}
+	}
+	return 0; 
+} 
+
+void advance(int horseNum, int* horses){ 
+	int coin = rand() % 2; 
+	if (coin == 1) { 
+		horses[horseNum] ++;
+	}	
+} 
+
+void printLane( int horseNum, int* horses) { 
+	for (int position = 0; position < 15; position++) { 
+		if (position == horses[horseNum]) { 
+			std::cout << horseNum; 
+		} 
+		else {
+			std::cout << "."; 
+		} 
+	}
+	std::cout << std::endl; 
+}
+
+bool isWinner(int horseNum, int* horses) { 
+	bool won = false; 
+	if (horses[horseNum] == 15) {
+		won = true; 
+		std::cout << "Horse " << horseNum << " WIN'S!" << std::endl; 
+	}
+	return won; 
+}
+
+
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # CS121-Procedural-Horse-Race-Project-
+## Algorithm
+```
+main() 
+    - array for 5 horses and each horse start from 0 
+    - keepGoing = true 
+    - srand(time(NULL)) random number generator 
+    
+    - while KeepGoing true 
+            go through each 5 horses 
+                advaance current horse 
+                printLane
+                check if current horse win 
+                    if it won 
+                        keepGoing = false 
+            if KeepGoing true 
+                user press enter for another turn 
+                press enter to continue 
+``` 
+advance () 
+    -  horse number and the horses array 
+    -  coin a random value 0 or 1 
+    -  if coin is 1 
+        move that horse forward one position  
+```
+printLane() 
+    - get horse number and the array
+    - go through 15 position 
+        if current position match the horse position 
+            print horse number 
+        if not 
+            print (.) 
+``` 
+isWinner() 
+    - won to false 
+    - get the horse number and the horse array 
+    
+    - if the horse postion reach 15 
+            won to true
+            print which horse won 
+    return won 
+```
+
+
+
+
+
